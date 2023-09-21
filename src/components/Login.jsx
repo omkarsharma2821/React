2873c7330fa7cd3195797a59fb051244bd309893
Omkar Sharma <omkarsharma2821@gmail.com>
2023-09-21 22:28:01 +0530

    changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -37,6 +37,9 @@ const Login = ({setLoggedIn}) => {
           title : 'Login success'
         })
         setLoggedIn(true);
+        const data = await res.json();
+        console.log(data);
+        sessionStorage.setItem('user' ,JSON.stringify(data));
        } else if(res.status === 400){
         Swal.fire({
           icon : 'error',
