56d5d71aa005fd8de56483950a49e1772c514b57
Omkar Sharma <omkarsharma2821@gmail.com>
2023-10-01 18:08:33 +0530

    ..

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -3,7 +3,7 @@ import useCartContext from "../CartContext";
 const CartPage = () => {
 const {cartitems, removeItemFromCart} = useCartContext();
 const diaplayCartItems = () =>{
-    if(!cartitems.length) return <h1 className="'text-center display-4 text-muted">
+    if(!cartitems.length) return <h1 className="text-center display-4 text-muted">
         No Items in Cart
     </h1>
     return <table className="table">
